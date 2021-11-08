23f1e0191489d694a5a5dc6875ef7f46043672bc
Samed Kahyaoglu <32146055+urtuba@users.noreply.github.com>
2021-11-08 19:44:04 +0300

    Add saver and getter methods for tweets

--- contract/tweet-verifier.sol
+++ contract/tweet-verifier.sol
@@ -1,4 +1,5 @@
 pragma solidity >=0.4.0 <0.6.0;
+pragma experimental ABIEncoderV2;
 
 contract TweetVerifier  {
     
@@ -27,5 +28,28 @@ contract TweetVerifier  {
     
     address [] authorized;
     mapping (bytes32 => TweetRecord) records;
+    
+        
+    // Functionality
+    
+    function saveTweet(string memory id, uint time, string memory message, string memory authorName, string memory authorNick, bool authorVerified) 
+    public returns (bytes32)
+    {
+        // create id by using tweet data's hash
+        bytes32 recordId = keccak256(abi.encodePacked(id, time, message, authorNick));
+        
+        // init and save TweetRecord
+        Author memory author = Author(authorName, authorNick, authorVerified);
+        Tweet memory tweet = Tweet(id, time, message, author);
+        records[recordId] = TweetRecord(now, tweet, msg.sender);
+        
+        // return its id
+        return recordId;
+    }
+    
+    
+    function getTweet(bytes32 recordId) public view returns (TweetRecord memory) {
+        return records[recordId];
+    }
     
 }
